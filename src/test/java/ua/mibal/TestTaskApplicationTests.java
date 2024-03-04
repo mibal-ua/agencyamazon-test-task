beeee56e564e7df81a670081fd7568784c651923
Mykhailo Balakhon <9mohapx9@gmail.com>
2024-03-04 08:53:22 +0100

    Init version

---
+++ src/test/java/ua/mibal/TestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.mibal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
