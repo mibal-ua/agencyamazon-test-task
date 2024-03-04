0add3b17fc745b9fb86607943820698e8c3a01bc
Mykhailo Balakhon <9mohapx9@gmail.com>
2024-03-04 09:04:13 +0100

    Implement `User` entity

---
+++ src/main/java/ua/mibal/domain/User.java
@@ -0,0 +1,53 @@
+package ua.mibal.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * @author Mykhailo Balakhon
+ * @link <a href="mailto:9mohapx9@gmail.com">9mohapx9@gmail.com</a>
+ */
+@Getter
+@Setter
+@Document("users")
+public class User implements UserDetails {
+
+    private Long id;
+
+    private String name;
+
+    private String username;
+
+    private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(() -> "USER");
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
