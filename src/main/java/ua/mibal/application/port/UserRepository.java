fd32bbf084d3a2b6dbc2cf3b2b0e9c4b79f42c1b
Mykhailo Balakhon <9mohapx9@gmail.com>
2024-03-04 09:36:49 +0100

    Create `UserRepository`

---
+++ src/main/java/ua/mibal/application/port/UserRepository.java
@@ -0,0 +1,14 @@
+package ua.mibal.application.port;
+
+import ua.mibal.domain.User;
+
+import java.util.Optional;
+
+/**
+ * @author Mykhailo Balakhon
+ * @link <a href="mailto:9mohapx9@gmail.com">9mohapx9@gmail.com</a>
+ */
+public interface UserRepository {
+
+    Optional<User> findByUsername(String username);
+}
