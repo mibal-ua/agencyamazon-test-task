cb7ac8cbe5eeadae01ee0c69208ca1d7f86dfda8
Mykhailo Balakhon <9mohapx9@gmail.com>
2024-03-05 10:01:37 +0100

    Add logging to `ReadFromJsonScheduledConfig`

--- src/main/java/ua/mibal/config/ReadFromJsonScheduledConfig.java
+++ src/main/java/ua/mibal/config/ReadFromJsonScheduledConfig.java
@@ -1,8 +1,10 @@
 package ua.mibal.config;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import ua.mibal.application.ReportJsonImportService;
 import ua.mibal.config.properties.MongoSourceProps;
@@ -13,6 +15,7 @@ import static java.util.concurrent.TimeUnit.MINUTES;
  * @author Mykhailo Balakhon
  * @link <a href="mailto:9mohapx9@gmail.com">9mohapx9@gmail.com</a>
  */
+@Slf4j
 @RequiredArgsConstructor
 @Configuration
 @EnableScheduling
@@ -26,5 +29,6 @@ public class ReadFromJsonScheduledConfig {
     @Scheduled(fixedDelay = 1, timeUnit = MINUTES)
     public void updateReportsFromJson() {
         reportJsonImportService.importFrom(mongoSourceProps.path());
+        log.info("Load from " + mongoSourceProps.path());
     }
 }
