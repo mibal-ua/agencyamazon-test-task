ffc8f06f9a13fa21db8328e873c42c41037bf6e3
Mykhailo Balakhon <9mohapx9@gmail.com>
2024-03-05 10:25:56 +0100

    Use `Date` at `SalesAndTrafficByDate` model

--- src/main/java/ua/mibal/domain/SalesAndTrafficByDate.java
+++ src/main/java/ua/mibal/domain/SalesAndTrafficByDate.java
@@ -2,9 +2,11 @@ package ua.mibal.domain;
 
 import lombok.Data;
 
+import java.util.Date;
+
 @Data
 public class SalesAndTrafficByDate {
-    private String date;
+    private Date date;
     private SalesByDate salesByDate;
     private TrafficByDate trafficByDate;
 }
